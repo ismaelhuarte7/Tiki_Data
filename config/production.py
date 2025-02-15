4decd87af1c57b8d84cfe87dcfbbc7ff0316bf01
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-15 19:50:29 -0300

    new db connection

--- config/production.py
+++ config/production.py
@@ -2,13 +2,15 @@ import os
 
 class Config:
     DEBUG = False
-    SESSION_TYPE = "filesystem"  
+    SESSION_TYPE = "filesystem"
     db_user = os.getenv("AZURE_SQL_USER")
     db_password = os.getenv("AZURE_SQL_PASSWORD")
     db_host = os.getenv("AZURE_SQL_SERVER")
     db_name = os.getenv("AZURE_SQL_DATABASE")
-    db_port = os.getenv("AZURE_SQL_PORT")
+    db_port = os.getenv("AZURE_SQL_PORT", 1433)
 
-    print(f"Database URI Producrion: {db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+    print ("host: ----------- ", db_host)
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    print(f"Database URI Production: {db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+
+    SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?driver=ODBC+Driver+17+for+SQL+Server"
