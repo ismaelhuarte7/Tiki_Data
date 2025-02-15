07f1c91270ad78bca6552be5ddad9fb09116f203
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-15 19:37:14 -0300

    db bug fix

--- config/production.py
+++ config/production.py
@@ -5,7 +5,7 @@ class Config:
     SESSION_TYPE = "filesystem"  
     db_user = os.getenv("AZURE_SQL_USER")
     db_password = os.getenv("AZURE_SQL_PASSWORD")
-    db_host = os.getenv("AZURE_SQL_HOST")
+    db_host = os.getenv("AZURE_SQL_SERVER")
     db_name = os.getenv("AZURE_SQL_DATABASE")
     db_port = os.getenv("AZURE_SQL_PORT")
 
