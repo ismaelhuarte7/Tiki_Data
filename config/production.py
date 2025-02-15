995666e28cfb78191722bdd8b9f0251f6383417d
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-15 16:48:56 -0300

    DB CONFIG

--- config/production.py
+++ config/production.py
@@ -3,9 +3,10 @@ import os
 class Config:
     DEBUG = False
     SESSION_TYPE = "filesystem"  
-    db_user = os.getenv("AZURE_MYSQL_USER")
-    db_password = os.getenv("AZURE_MYSQL_PASSWORD")
-    db_host = os.getenv("AZURE_MYSQL_HOST")
-    db_name = os.getenv("AZURE_MYSQL_NAME")
+    db_user = os.getenv("AZURE_SQL_USER")
+    db_password = os.getenv("AZURE_SQL_PASSWORD")
+    db_host = os.getenv("AZURE_SQL_HOST")
+    db_name = os.getenv("AZURE_SQL_DATABASE")
+    db_port = os.getenv("AZURE_SQL_PORT")
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
