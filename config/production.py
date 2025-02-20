2f001b1dcb0a3bbc332582a736128695e56a8339
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-20 12:47:18 -0300

    cambio bd produccion

--- config/production.py
+++ config/production.py
@@ -3,19 +3,8 @@ import os
 class Config:
     DEBUG = False
     SESSION_TYPE = "filesystem"
-    db_user = os.getenv("AZURE_SQL_USER")
-    db_password = os.getenv("AZURE_SQL_PASSWORD")
-    db_host = os.getenv("AZURE_SQL_SERVER")
-    db_name = os.getenv("AZURE_SQL_DATABASE")
-    db_port = os.getenv("AZURE_SQL_PORT", 1433)
 
-    print ("host: ----------- ", db_host)
+    # Obtiene la URL completa desde la variable de entorno
+    SQLALCHEMY_DATABASE_URI = os.getenv("POSTGRES_URL")
 
-    print(f"Database URI Production: {db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
-
-    SQLALCHEMY_DATABASE_URI = (
-    f"mssql+pyodbc://{db_user}:{db_password}@{db_host}:{db_port}/"
-    f"{db_name}?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
-)
-
-    
+    print(f"Database URI Production: {SQLALCHEMY_DATABASE_URI}")
