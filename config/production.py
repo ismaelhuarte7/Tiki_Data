1cf6e69c358495036f5c9962f9b18eeda7d1525e
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-24 21:41:44 -0300

    keys

--- config/production.py
+++ config/production.py
@@ -4,8 +4,8 @@ class Config:
     DEBUG = False
     SESSION_TYPE = "filesystem"
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    MAILJET_API_KEY = '882491176762670da1ba3ecd96a2af7f'
-    MAILJET_SECRET_KEY = '4db10d01bb3315ce8ca6da7df4a832d2'
+    MAILJET_API_KEY = os.getenv("MAILJET_API_KEY")
+    MAILJET_SECRET_KEY = os.getenv("MAILJET_SECRET_KEY")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_UNPOOLED")
     BASE_URL = os.getenv("BASE_URL")
