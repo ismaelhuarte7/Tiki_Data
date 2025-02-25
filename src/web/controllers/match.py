e0fe11e08dda729ccba6002867d7d364bd5a6c94
Tobias Napoli <tobiasnapoli03@gmail.com>
2025-02-25 18:09:05 -0300

    player goals

--- src/web/controllers/match.py
+++ src/web/controllers/match.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from src.models import Match
 from src.models import Court
 from src.models import Player
+from src.models import Goal
 
 bp = Blueprint("match", __name__, url_prefix="/match")
 
@@ -15,8 +16,26 @@ def create():
     if request.method == "POST":
         date = request.form["match_date"]
         court_id = request.form["court_id"]
+        
+        # Crear el partido
         match = Match.create(date, court_id)
+        
+        # Procesar los goles de los jugadores del Equipo 1
+        for i in range(1, 6):
+            player_id = request.form.get(f"player1_{i}")
+            goals = int(request.form.get(f"goals_player1_{i}"))
+            for _ in range(goals):
+                Goal.create(player_id=player_id, match_id=match.id)
+        
+        # Procesar los goles de los jugadores del Equipo 2
+        for i in range(1, 6):
+            player_id = request.form.get(f"player2_{i}")
+            goals = int(request.form.get(f"goals_player2_{i}"))
+            for _ in range(goals):
+                Goal.create(player_id=player_id, match_id=match.id)
+        
         return redirect(url_for("match.create"))
+    
     courts = Court.get_all_courts()
     players = Player.get_all_players()
     return render_template("match/create.html", courts=courts, players=players)
