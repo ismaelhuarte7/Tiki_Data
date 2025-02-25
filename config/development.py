c580ca2da5897cf608aff6e46fb0ecdabfb141a8
ismaelhuarte <ismaelhuarte7@gmail.com>
2025-02-25 15:12:45 -0300

    key secret

--- config/development.py
+++ config/development.py
@@ -5,8 +5,8 @@ class Config:
     SESSION_TYPE = "redis"  
     SESSION_REDIS = redis.from_url(os.environ.get("REDIS_URL"))
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    MAILJET_API_KEY = '882491176762670da1ba3ecd96a2af7f'
-    MAILJET_SECRET_KEY = '4db10d01bb3315ce8ca6da7df4a832d2'
+    MAILJET_API_KEY = os.environ.get("MAILJET_API_KEY")
+    MAILJET_SECRET_KEY = os.environ.get("MAILJET_SECRET_KEY")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
     BASE_URL = os.getenv("BASE_URL")
     SESSION_COOKIE_SECURE = True
