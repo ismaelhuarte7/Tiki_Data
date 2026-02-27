a07fdae29918ca844af70fc1838eb6f244fc5946
ismaelhuarte7 <ismaelhuarte7@gmail.com>
2026-02-27 16:19:26 -0300

    railway configg

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 
 # Cargar variables de entorno ANTES de importar Config
@@ -9,4 +10,5 @@ from config.config import Config
 app = create_app(Config)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
